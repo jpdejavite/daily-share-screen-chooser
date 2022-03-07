80aec8a395c2a9adaf30c959ee2bbf90deae3978
Joao Araujo <jpdejavite@gmail.com>
2022-03-07 19:16:43 -0300

    chore: inital commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# daily-share-screen-chooser
+
+A NodeJS simple API to choose for a daily a user to share screen
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) and the [Heroku CLI](https://cli.heroku.com/) installed.
+
+```sh
+$ git clone https://github.com/heroku/node-js-getting-started.git # or clone your own fork
+$ cd node-js-getting-started
+$ npm install
+$ npm start
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3000
+// Require the Bolt for JavaScript package (github.com/slackapi/bolt)
+const { App, LogLevel } = require("@slack/bolt");
+
+express()
+  .use(bodyParser.json())
+  .get('/', (req, res) => res.render('pages/index'))
+  .post('/profile', (req, res, next) => {
+    console.log(req.body)
+    res.json(req.body)
+  })
+  .post('/slack/message-sent', async (req, res) => {
+    if (!req || !req.body) {
+      return res.status(200).send({});
+    }
+
+    if (req.body.challenge) {
+      return res.status(200).send(req.body.challenge);
+    }
+
+    if (req.body.type === 'message') {
+
+      // {
+      //   "type": "message",
+      //   "channel": "C2147483705",
+      //   "user": "U2147483697",
+      //   "text": "Hello world",
+      //   "ts": "1355517523.000005"
+      // }
+      console.log(req.body);
+    }
+
+    return res.status(200).send({});
+
+    // const token = 'xoxb-399131229937-2458808280577-lPT31ezH9VuVCWOS8aBZJfF4';
+    // const app = new App({
+    //   token,
+    //   signingSecret: "52aff634a2c57d88be21f462743dcd77",
+    //   // LogLevel can be imported and used to make debugging simpler
+    //   logLevel: LogLevel.DEBUG
+    // });
+
+    // // ID of the channel you want to send the message to
+    // const channelId = "C035TSC55RU";
+
+    // try {
+    //   // // Call the chat.postMessage method using the built-in WebClient
+    //   const result = await app.client.chat.postMessage({
+    //     // The token you used to initialize your app
+    //     token,
+    //     channel: channelId,
+    //     text: 'Hello World'
+    //     // You could also use a blocks[] array to send richer content
+    //   });
+
+    //   // Print result, which includes information about the message (like TS)
+    //   console.log(result);
+    // }
+    // catch (error) {
+    //   console.error(error);
+    // }
+
+    // return res.status(200).send({});
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
